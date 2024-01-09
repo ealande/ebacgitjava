d7834b0151b14235ae3789beaaf15b3a6e8e8803
ealande <erickalan23@gmail.com>
2024-01-09 15:29:09 -0300

    ebac aula

---
+++ Mavenproject1.java
@@ -0,0 +1,12 @@
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author erickalan
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
