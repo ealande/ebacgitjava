e6143fbb240b1e0388466ffbaeff1e379faf897c
ealande <erickalan23@gmail.com>
2024-01-10 14:39:01 -0300

    exe class

---
+++ exeebacclass/src/Cachorro.java
@@ -0,0 +1,39 @@
+//Classe contendo características de um cachorro
+public class Cachorro {
+       private String raca;
+        private String cor;
+        private int idade;
+        private int peso;
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+}
