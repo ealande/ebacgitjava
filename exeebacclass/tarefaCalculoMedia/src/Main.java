daedc77e4c960e8705d35cfcdebf01b4c1365af8
ealande <erickalan23@gmail.com>
2024-01-10 21:56:10 -0300

    tarefaMedia

---
+++ exeebacclass/tarefaCalculoMedia/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+    System.out.println(imprimirMedia());
+    }
+    public static float imprimirMedia() {
+        float nota1 = 2;
+        float nota2 = 6;
+        float nota3 = 8;
+        float nota4 = 5;
+        return(nota1+nota2+nota3+nota4)/4;
+
+    }
+}
