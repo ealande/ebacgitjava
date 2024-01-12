25a3f9b63972f547a91a93396391cad9e70c81f1
ealande <erickalan23@gmail.com>
2024-01-12 13:34:34 -0300

    wrapper

---
+++ exeebacclass/tarefa_wrapper/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        long teste = 3000000000L;
+        System.out.println(teste);
+
+        Short teste2 = (short) teste;
+        System.out.println(teste2);
+    }
+    }
